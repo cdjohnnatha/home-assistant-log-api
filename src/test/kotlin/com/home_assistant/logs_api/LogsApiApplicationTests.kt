9084839d950bb0985f4057d9d1395ee0ceb1048b
claudio <claudio.lourenco@factorypal.com>
2025-07-14 16:51:57 -0300

    create project initial structure

---
+++ src/test/kotlin/com/home_assistant/logs_api/LogsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.home_assistant.logs_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LogsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
